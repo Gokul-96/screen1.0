3e72f4685a56ae64415b5afd7859643ffa06e927
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-09-08 18:53:53 +0530

    step 4 and 5

--- src/assets/components/SegmentPopup.jsx
+++ src/assets/components/SegmentPopup.jsx
@@ -2,10 +2,28 @@ import { useState } from 'react';
 
 function SegmentPopup({ closePopup }) {
   const [segmentName, setSegmentName] = useState('');
-
+   const [selectedSchema, setSelectedSchema] = useState('');
 
      const handleSegmentName = (e) => {
     setSegmentName(e.target.value);
+  };
+
+
+  //schema already provided
+  const schema = [
+
+    { label: 'First Name', value: 'first_name' },
+    { label: 'Last Name', value: 'last_name' },
+    { label: 'Gender', value: 'gender' },
+    { label: 'Age', value: 'age' },
+    { label: 'Account Name', value: 'account_name' },
+    { label: 'City', value: 'city' },
+    { label: 'State', value: 'state' }
+  ];
+
+
+  const handleSchemaChange = (e) => {
+    setSelectedSchema(e.target.value);
   };
 
 
@@ -17,7 +35,7 @@ function SegmentPopup({ closePopup }) {
       <div className="popup-content">
         <button onClick={closePopup}>Close</button>
 
-        {/* Input*/}
+        {/* Input Enter*/}
         <p>Enter the Name of the Segment:</p>
         <input 
           type="text" 
@@ -26,6 +44,31 @@ function SegmentPopup({ closePopup }) {
           onChange={handleSegmentName} 
         />
         {/* <p>Segment Name: {segmentName}</p> */}
+    
+    
+    {/* {dropdown} */}
+    <p>Add schema to segment:</p>
+
+    
+    <select value={selectedSchema} onChange={handleSchemaChange}>
+          <option value="" >Select schema </option>
+
+          {schema.map(option => (
+            <option key={option.value} >
+              {option.label}
+            </option>
+          ))}
+        </select>
+
+
+
+    <button onClick={() => {  }}>
+
+          + Add new schema
+</button>
+    
+    
+    
       </div>
     </div>
   );
