815fb0bcc293b390c41f63515ab01b9e00e0d1dd
Gokul-96 <gokulakrishnan.m96@gmail.com>
2024-09-08 11:28:55 +0530

    toggle implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
-
+import { useState } from 'react';
 const App = () => {
+  const [popup, setPopup] = useState(false)
+  const togglePopup = () => {
+    setPopup(!popup)
+  };
+
+
   return (
     <div className='App'>
 
-<button>Save Segment</button>
+<button onClick={togglePopup}>Save Segment</button>
+
+{/* 1. Conditionally render popup */}
 
 
+{popup && (
+        <div className="popup">
+          <div className="popup-content">
+            <button onClick={togglePopup}>Close</button>
+            <p>popup here</p>
+          </div>
+        </div>
+      )}
 
     </div>
   )
